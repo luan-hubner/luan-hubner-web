7f59293b3c09fb509840abe6a156e104cc4d49a7
luan-hubner <luanhubner.pro@gmail.com>
2022-01-19 23:06:12 -0400

    add page of first article

--- src/index.js
+++ src/index.js
@@ -13,13 +13,17 @@ import About from './pages/about';
 import Articles from './pages/articles';
 import Projects from './pages/projects';
 
+import AC001 from './pages/articles/pages/voce-ja-se-sentiu-enjoado-ou-cansado-de-um-projeto-no-meio-do-caminho';
+
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
       <Route path="/">
         <Route index element={<Home />} />
         <Route path="about" element={<About />}></Route>
-        <Route path="articles" element={<Articles />}></Route>
+        <Route path="articles" element={<Articles />}>
+          <Route path="voce-ja-se-sentiu-enjoado-ou-cansado-de-um-projeto-no-meio-do-caminho" element={<AC001 />} />
+        </Route>
         <Route path="projects" element={<Projects />}></Route>
       </Route>
     </Routes>
---
+++ src/pages/articles/pages/voce-ja-se-sentiu-enjoado-ou-cansado-de-um-projeto-no-meio-do-caminho/index.js
@@ -0,0 +1,7 @@
+import s from '../style.module.css';
+
+export default function AC001() {
+  return (
+    <div>sd</div>
+  );
+}
